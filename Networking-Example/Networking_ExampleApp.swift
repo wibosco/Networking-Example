e91a164300608ed38b70c00dc86d808cb9cec40c
William Boles <wibosco@users.noreply.github.com>
2023-01-27 10:41:05 +0000

    Initial project structure

---
+++ Networking-Example/Networking_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Networking_ExampleApp.swift
+//  Networking-Example
+//
+//  Created by William Boles on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Networking_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
